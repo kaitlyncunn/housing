a11b610e88d1dc53d02c869d95bce5be281f4eca
kaitlyncunn <105887765+kaitlyncunn@users.noreply.github.com>
2022-05-19 16:37:11 -0400

    Add files via upload

---
+++ assignment10.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+
+df = pd.read_csv('housing.csv')
+
+st.title('California Housing Date (1990) by Kaitlyn Cunningham')
+
+median_housing_price_filter = st.slider('choose min. house price', 0,500001,20)
+
+df = df[df.median_house_value>median_housing_pirce_filter]
+
+location_filter = st.sidebar.mulitselect('location filter', df.ocean_proximity.unique(), df.ocean_proximity.unique())
+
+df = df[df.ocean_proximity.isin(location_filter)]
+
+income - st.sidebar.radio(
+    "select income level",
+    ('low', 'medium', 'high'))
+
+if income == 'low':
+        df = df[df.median_income <= 2.5],
+elif income == 'medium':
+        df = df[df.median_income > 2.5 and df.median_income <+ 4.5],
+else:
+        df = df[df.median_income > 4.5]
+        
+st.map(df)
+
+fig, ax = plt.subplots (figsize = (20, 5))
+df.median_house_value.hist (ax=ax, bins=30)
+st.pyplot(fig)
